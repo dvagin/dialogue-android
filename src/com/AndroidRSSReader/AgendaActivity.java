b079f5375ca451159d4a8bc2c3a9d0ad8f50baeb
Vsevolod Sevostyanov <v.sevostyanov@rocknglory.ru>
2012-09-26 21:18:37 +0400

    Добавлено немного дизайна исправлены ошибки

--- src/com/AndroidRSSReader/AgendaActivity.java
+++ src/com/AndroidRSSReader/AgendaActivity.java
@@ -14,35 +14,41 @@ public class AgendaActivity extends TabActivity {
 		TabHost tabs = getTabHost();
         TabSpec day1 = tabs.newTabSpec("1");
         Intent day1in = new Intent(this,AgendaTab.class);
+        day1in.putExtra("HallNum", 1);
         day1.setIndicator("1");
         day1.setContent(day1in);
         TabSpec day2 = tabs.newTabSpec("2");
         Intent day2in = new Intent(this,AgendaTab.class);
+        day2in.putExtra("HallNum", 2);
         day2.setIndicator("2");
         day2.setContent(day2in);
         TabSpec day3 = tabs.newTabSpec("3");
         Intent day3in = new Intent(this,AgendaTab.class);
+        day3in.putExtra("HallNum", 3);
         day3.setIndicator("3");
         day3.setContent(day3in);
         TabSpec day4 = tabs.newTabSpec("4");
         Intent day4in = new Intent(this,AgendaTab.class);
+        day4in.putExtra("HallNum", 4);
         day4.setIndicator("4");
         day4.setContent(day4in);
         TabSpec day5 = tabs.newTabSpec("5");
         Intent day5in = new Intent(this,AgendaTab.class);
+        day5in.putExtra("HallNum", 5);
         day5.setIndicator("5");
         day5.setContent(day5in);
-        TabSpec day6 = tabs.newTabSpec("6");
+        /*TabSpec day6 = tabs.newTabSpec("6");
         Intent day6in = new Intent(this,AgendaTab.class);
+        day6in.putExtra("HallNum", 6);
         day6.setIndicator("6");
-        day6.setContent(day6in);
+        day6.setContent(day6in);*/
         
         tabs.addTab(day1);
         tabs.addTab(day2);
         tabs.addTab(day3);
         tabs.addTab(day4);
         tabs.addTab(day5);
-        tabs.addTab(day6);
+       // tabs.addTab(day6);
 	}
 
 }
--- src/com/AndroidRSSReader/AgendaTab.java
+++ src/com/AndroidRSSReader/AgendaTab.java
@@ -117,20 +117,24 @@ public class AgendaTab extends Activity {
 	    public boolean isChildSelectable(int groupPosition, int childPosition) {
 	        return true;
 	    }
+	    
 	};
 	
+	protected ArrayList<ArrayList<DateItem>> groups;
+	protected ExpListAdapter adapter;
+	ExpandableListView listView;
+	
+	public int hallNum;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.agendatab);
-		ExpandableListView listView = (ExpandableListView)findViewById(R.id.expandableListView1);
+		listView = (ExpandableListView)findViewById(R.id.expandableListView1);
 		refresh();
-		
-		ArrayList<ArrayList<DateItem>> groups = new ArrayList<ArrayList<DateItem>>();
+		hallNum = getIntent().getExtras().getInt("HallNum");
+		groups = new ArrayList<ArrayList<DateItem>>();
         ArrayList<DateItem> children1 = new ArrayList<DateItem>();
         ArrayList<DateItem> children2 = new ArrayList<DateItem>();
-        ArrayList<DateItem> children3 = new ArrayList<DateItem>();
-        ArrayList<DateItem> children4 = new ArrayList<DateItem>();
         children1.add(new DateItem("9:00-13:00 Opening plenary session - \"Socio-political transformation: chance or challenge for dialogue?\"","October 4"));
         children1.add(new DateItem("15:00-19:00 Thematic plenary session 1 - \"Global Peace and Justice\"","October 4"));
         groups.add(children1);
@@ -138,8 +142,11 @@ public class AgendaTab extends Activity {
         /*children2.add("Child_2");
         children2.add("Child_3");*/
         groups.add(children2);     
-        ExpListAdapter adapter = new ExpListAdapter(getApplicationContext(), groups);
+        adapter = new ExpListAdapter(getApplicationContext(), groups);
+        
         listView.setAdapter(adapter);
+        
+        //adapter.notifyDataSetChanged();
         
         
 		
@@ -149,14 +156,40 @@ public class AgendaTab extends Activity {
 		InternetReader read = new InternetReader(){
 			public void reqFinished(String answer){
 				Log.d("tag",answer);
+				ArrayList<ArrayList<DateItem>> newgroups = new ArrayList<ArrayList<DateItem>>();
 				Document doc = InternetReader.getDomElement(answer);
+				doc.getDocumentElement().normalize();
 				   NodeList nl = doc.getElementsByTagName("Hall");
-				   NodeList nlc = nl.item(0).getChildNodes();
+				   NodeList nlc = nl.item(hallNum-1).getChildNodes();
 				   /*NodeList nlcl = nlc.item(1).getChildNodes();*/
 				   Log.d("tag","length "+nlc.getLength());
 				   for(int i = 0;i<nlc.getLength();i++){
-					   Log.d("ekv","name "+nlc.item(i).getNodeName()/*+" value "+InternetReader.getValue((Element)nl.item(0),nlc.item(i).getNodeName())*/);
+					   if(!nlc.item(i).getNodeName().equals("#text")){
+						   NodeList inDate = nlc.item(i).getChildNodes();
+						   Log.d("indate","elems length "+inDate.getLength());
+						   ArrayList<DateItem> buf = new ArrayList<DateItem>();
+						   for(int j = 0; j<inDate.getLength();++j){
+							   if(!inDate.item(j).getNodeName().equals("#text")){
+								   buf.add(new DateItem(inDate.item(j).getTextContent(),((Element)nlc.item(i)).getAttribute("value")));
+								   Log.d("indate","name "+inDate.item(j).getNodeName() + " " + ((Element)nlc.item(i)).getAttribute("value") + " val " + inDate.item(j).getTextContent()); 
+							   }
+						   }
+						   if(buf.size()==0){
+							   buf.add(new DateItem("There are no tables and no sessions.",((Element)nlc.item(i)).getAttribute("value")));
+						   }
+						   newgroups.add(buf);
+					   }
 				   }
+				   Log.d("tag","finished " + newgroups.size());
+				   groups.clear();
+				   groups.addAll(newgroups);
+				   AgendaTab.this.runOnUiThread(new Runnable(){
+					  public void run(){
+						  //listView.invalidate();
+						  adapter.notifyDataSetChanged();
+					  }
+				   });
+				   
 			}
 		};
 		if(InternetReader.checkConnection(getBaseContext())){
